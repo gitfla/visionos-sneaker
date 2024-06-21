8d00764a40989890b3b31a67e6e0a4bc93cdda6b
Flavio <flaviofiszman@gmail.com>
2024-06-21 10:33:17 -0300

    Initial Commit

---
+++ aula2106.vivo/aula2106_vivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula2106_vivoApp.swift
+//  aula2106.vivo
+//
+//  Created by F F on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct aula2106_vivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
